f93a3b59bfb9ab355f3bbcff4ee811ca4da30cde
rhkanrdl <emrdl7@gmail.com>
2016-11-02 09:40:38 +0900

    메인,서브,리스트 코딩완료

--- js/common.js
+++ js/common.js
@@ -34,28 +34,44 @@ $(document).ready(function() {
           pager:false
       });
 
-      $(".navigation p a").on('click', function() {
-          $(".navigation .category ul").css("display", "none")
-          $(this).parent().parent().find("ul").css("display", "block");
+      $('.navigation p a').on('click', function() {
+          $('.navigation .category ul').css('display', 'none')
+          $(this).parent().parent().find('ul').css('display', 'block');
       });
 
-        $("section.product .sort p a").on('click', function() {
-            $("section.product .sort p a").removeClass("active");
-            $(this).addClass("active");
-            $("section.product .container #list").removeClass();
-            $("section.product .container #list").addClass($(this).attr("data"));
+        $('section.product .sort p a').on('click', function() {
+            $('section.product .sort p a').removeClass('active');
+            $(this).addClass('active');
+            $('section.product .container #list').removeClass();
+            $('section.product .container #list').addClass($(this).attr('data'));
         });
 
-        $("div.select input").on('click', function() {
-            $(".info-text .select ul").css('display', 'none');
+        $('div.select input').on('click', function() {
+            $('.info-text .select ul').css('display', 'none');
             $(this).parent().find('ul').css('display','block');
 
         });
-        $("div.select li a").on('click', function() {
-            $(".info-text .select ul").css('display', 'none');
-            $(this).parent().parent().parent().find('input').attr("value", $(this).text());
+        $('div.select li a').on('click', function() {
+            $('.info-text .select ul').css('display', 'none');
+            $(this).parent().parent().parent().find('input').attr('value', $(this).text());
 
         });
+
+        $('.stars button').on('click', function() {
+            var starValue = $(this).attr("data");
+             $(".stars button").find("i").removeClass("on");
+             $(".stars button").each(function() {
+                 if ($(this).attr("data") <= starValue) {
+                     $(this).find("i").addClass("on");
+                 }
+             });
+        });
+
+        $('.btn-area.comment button').on('click', function() {
+            $(this).parent().parent().find('.reply-write').css('display','block');
+
+        });
+
 
 
 });
